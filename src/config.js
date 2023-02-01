c97a7bfcaf4ec3de8573e5b85e8a330e3edad3c4
Rayhan-abdullha <captainrayhan215@.com>
2023-02-01 20:14:14 +0600

    some fetures update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 import axios from "axios";
+// https://blog24-server-app.onrender.com/api/
 
 export const axiosInstance = axios.create({
-    baseURL: "https://blog24-server-app.onrender.com/api/"
-})
+  baseURL: "https://blog24-server.vercel.app/api/",
+});
